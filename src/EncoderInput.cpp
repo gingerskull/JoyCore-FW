d6cc8dbd90770d3eafac5748ab039aa03d6d3399
gingerskull <david.ri@gmail.com>
2025-07-09 21:11:02 +0300

    Add SPDX license identifier

--- src/EncoderInput.cpp
+++ src/EncoderInput.cpp
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 #include "EncoderInput.h"
 #include "JoystickWrapper.h"
 #include "Config.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 /*
  * Custom Button Box Controller for Flight Simulators and Gaming
  * 
